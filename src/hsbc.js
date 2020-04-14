15eed58229f2798b88f41e473343b0257be71584
Jacky Chan <chankongching@gmail.com>
2020-04-14 14:44:37 +0800

    Debug

--- src/hsbc.js
+++ src/hsbc.js
@@ -33,7 +33,10 @@ casper.start(url, function() {
 casper.then(function() {
 // console.log($('#tools_form_1').html());
   this.echo(this.getTitle());
-  this.echo($('select.tools_form_1').html());
+  $(this).find('select').each(function(){
+    console.log($(this).html())
+  })
+  // this.echo($('select.tools_form_1').html());
  //填入form
  this.evaluate(function() {
    $('#zone').val('3').change();
